375fc547eab97af82b911fa059fb5d028847176f
KO Myung-Hun <komh@chollian.net>
2020-09-01 21:05:45 +0900

    Add LGPL v2.1 license
        modified:   klogger.c
        modified:   klogger.h
        modified:   mcdcaps.c
        modified:   mcdclose.c
        modified:   mcdcue.c
        modified:   mcddrvrt.c
        modified:   mcddrvsv.c
        modified:   mcdfuncs.c
        modified:   mcdinfo.c
        modified:   mcdload.c
        modified:   mcdopen.c
        modified:   mcdpause.c
        modified:   mcdplay.c
        modified:   mcdpos.c
        modified:   mcdproc.c
        modified:   mcdresume.c
        modified:   mcdseek.c
        modified:   mcdset.c
        modified:   mcdstat.c
        modified:   mcdstop.c
        modified:   mcdtemp.h

--- mcdstop.c
+++ mcdstop.c
@@ -1,3 +1,26 @@
+/****************************************************************************
+**
+** mcdstop.c
+**
+** Copyright (C) 2020 by KO Myung-Hun <komh@chollian.net>
+**
+** This file is part of K Soft Sequencer.
+**
+** $BEGIN_LICENSE$
+**
+** GNU Lesser General Public License Usage
+** This file may be used under the terms of the GNU Lesser
+** General Public License version 2.1 as published by the Free Software
+** Foundation and appearing in the file LICENSE.LGPL included in the
+** packaging of this file.  Please review the following information to
+** ensure the GNU Lesser General Public License version 2.1 requirements
+** will be met: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html.
+**
+**
+** $END_LICENSE$
+**
+****************************************************************************/
+
 /****************************************************************************/
 /*                                                                          */
 /* SOURCE FILE NAME:  MCDSTOP.C                                             */
